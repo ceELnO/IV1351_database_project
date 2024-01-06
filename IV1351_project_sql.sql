fc8cf9ea909907987f29b7a14073244196f43490
ceELnO <85216734+ceELnO@users.noreply.github.com>
2024-01-06 17:27:24 +0100

    Updated the entire project a bit

--- IV1351_project_sql.sql
+++ IV1351_project_sql.sql
@@ -1,193 +1,26 @@
 /* ------------------------------------------- create tables -------------------- */
 
+/* clear tables */
+
+DROP TABLE IF EXISTS Difficulty_level, Standard_price, Student, Instructor, 
+		Guardian_contact_details, Student_sibling, Instrument_type, 
+		Instructor_instrument, Rent_instrument_assortment, Rented_instrument, 
+		Individual_lesson, Group_lesson, Group_lesson_enrollment, Ensemble_type, 
+		Ensemble, Ensemble_enrollment CASCADE;
 
 /* ---------------------- complex order ----------------------- */
 
 /* "difficulty_name" contains: {begginer, intermidate, advanced} */
 
-CREATE TEMPORARY TABLE Difficulty_level(
+CREATE TABLE Difficulty_level(
 	difficulty_level_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
 	difficulty_name VARCHAR(50)
 );
 
-/* ------------------- People ----------------- */
+/* standard prices */
 
-CREATE TEMPORARY TABLE Student(
-	student_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-	student_fname VARCHAR(50),
-	student_lname VARCHAR(50),
-	student_address VARCHAR(50),
-	student_mail_address VARCHAR(50),
-	student_phone_number VARCHAR(50),
-	student_sibling BOOLEAN
-);
-CREATE TEMPORARY TABLE Instructor(
-	instructor_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-	instructor_fname VARCHAR(50),
-	instructor_lname VARCHAR(50),
-	instructor_address VARCHAR(50),
-	instructor_mail_address VARCHAR(50),
-	instructor_phone_number VARCHAR(50)
-);
-
-/* --- details with people ---*/
-
-CREATE TEMPORARY TABLE Guardian_contact_details(
-	student_id_id INT NOT NULL PRIMARY KEY,
-	guardian_fname VARCHAR(50),
-	guardian_lname VARCHAR(50),
-	guardian_mail_address VARCHAR(50),
-	guardian_phone_number VARCHAR(50),
-
-	CONSTRAINT guardian_student FOREIGN KEY(student_id_id)
-        REFERENCES Student(student_id)
-);
-
-/* student_sibling_group identifies groups of siblings */
-
-CREATE TEMPORARY TABLE Student_sibling(
-	student_id_id INT NOT NULL PRIMARY KEY,
-	student_sibling_group SERIAL,
-
-	CONSTRAINT student_sibling_grouping FOREIGN KEY(student_id_id)
-        REFERENCES Student(student_id)
-);
-	
-/* --------------------------- Instrument ---------------------------*/
-
-CREATE TEMPORARY TABLE Instrument_type(
-	instrument_type_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-	instrument_type_name VARCHAR(50),
-	instrument_type_description VARCHAR(1000)
-);
-
-/* interactions */
-
-CREATE TEMPORARY TABLE Instructor_instrument(
-	instructor_id_id INT,
-	instrument_type_id_id INT,
-	difficulty_level_id_id INT,
-
-	CONSTRAINT instructor_instrument FOREIGN KEY(instructor_id_id)
-        REFERENCES Instructor(instructor_id),
-	CONSTRAINT instructor_instrument_type FOREIGN KEY(instrument_type_id_id)
-        REFERENCES Instrument_type(instrument_type_id),
-	CONSTRAINT instructor_instrument_difficulty FOREIGN KEY(difficulty_level_id_id)
-        REFERENCES Difficulty_level(difficulty_level_id)
-);
-
-CREATE TEMPORARY TABLE Rent_instrument_assortment(
-	instrument_type_id_id INT PRIMARY KEY NOT NULL,
-	instrument_type_stored_quantity INT,
-	instrument_type_rent_cost INT
-);
-
-CREATE TEMPORARY TABLE Rented_instrument(
-	student_id_id INT NOT NULL,
-	instrument_type_id_id INT NOT NULL,
-	rented_instrument_since TIMESTAMP NOT NULL,
-	rented_instrument_turned_in TIMESTAMP,
-
-	CONSTRAINT student_renting_instrument FOREIGN KEY(student_id_id)
-        REFERENCES Student(student_id),
-	CONSTRAINT instructor_instrument_type FOREIGN KEY(instrument_type_id_id)
-        REFERENCES Instrument_type(instrument_type_id)
-);
-
-/* ------------------------ lesson / ensemble ---------------------- */
-
-/* individual lessons */
-
-CREATE TEMPORARY TABLE Individual_lesson(
-	individual_lesson_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-	instructor_id_id INT,
-	instrument_type_id_id INT,
-	difficulty_level_id_id INT,
-	student_id_id INT,
-	individual_lesson_start_time TIMESTAMP NOT NULL,
-	individual_lesson_end_time TIMESTAMP,
-	individual_lesson_student_price INT,
-	individual_lesson_instructor_price INT,
-
-	CONSTRAINT instructor_id_id FOREIGN KEY(instructor_id_id)
-        REFERENCES Instructor(instructor_id),
-	CONSTRAINT instrument_type_id_id FOREIGN KEY(instrument_type_id_id)
-        REFERENCES Instrument_type(instrument_type_id),
-	CONSTRAINT difficulty_level_id_id FOREIGN KEY(difficulty_level_id_id)
-        REFERENCES Difficulty_level(difficulty_level_id),
-	CONSTRAINT student_id_id FOREIGN KEY(student_id_id)
-        REFERENCES Student(student_id)
-);
-
-/* group lessons */
-
-CREATE TEMPORARY TABLE Group_lesson(
-	group_lesson_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-	instructor_id_id INT,
-	instrument_type_id_id INT,
-	difficulty_level_id_id INT,
-	group_lesson_start_time TIMESTAMP NOT NULL,
-	group_lesson_end_time TIMESTAMP,
-	group_lesson_student_price INT,
-	group_lesson_instructor_price INT,
-
-	CONSTRAINT instructor_id_id FOREIGN KEY(instructor_id_id)
-        REFERENCES Instructor(instructor_id),
-	CONSTRAINT instrument_type_id_id FOREIGN KEY(instrument_type_id_id)
-        REFERENCES Instrument_type(instrument_type_id),
-	CONSTRAINT difficulty_level_id_id FOREIGN KEY(difficulty_level_id_id)
-        REFERENCES Difficulty_level(difficulty_level_id)
-);
-
-CREATE TEMPORARY TABLE Group_lesson_enrollment(
-	group_lesson_id_id INT NOT NULL,
-	student_id_id INT NOT NULL,
-
-	CONSTRAINT group_lesson_id_id FOREIGN KEY(group_lesson_id_id)
-        REFERENCES Group_lesson(group_lesson_id),
-	CONSTRAINT student_id_id FOREIGN KEY(student_id_id)
-        REFERENCES Student(student_id)
-
-);
-
-/* ensembe */
-
-CREATE TEMPORARY TABLE Ensemble_type(
-	ensemble_type_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
-	ensemble_type_name VARCHAR(50),
-	ensemble_description VARCHAR(500)
-);
-
-CREATE TEMPORARY TABLE Ensemble(
-	ensemble_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
-	instructor_id_id INT NOT NULL,
-	ensemble_type_id_id INT NOT NULL,
-	ensemble_start_time TIMESTAMP NOT NULL,
-	ensemble_end_time TIMESTAMP,
-	ensemble_student_price INT,
-	ensemble_instructor_price INT,
-	ensemble_max_students INT,
-
-	CONSTRAINT instructor_id_id FOREIGN KEY(instructor_id_id)
-        REFERENCES Instructor(instructor_id),
-	CONSTRAINT ensemble_type_id_id FOREIGN KEY(ensemble_type_id_id)
-        REFERENCES Ensemble_type(ensemble_type_id)
-);
-
-CREATE TEMPORARY TABLE Ensemble_enrollment(
-	ensemble_lesson_id_id INT NOT NULL,
-	student_id_id INT NOT NULL,
-
-	CONSTRAINT ensemble_lesson_id_id FOREIGN KEY(ensemble_lesson_id_id)
-        REFERENCES Ensemble(ensemble_id),
-	CONSTRAINT student_id_id FOREIGN KEY(student_id_id)
-        REFERENCES Student(student_id)
-);
-
-/* ----------------------- standard prices ------------------------ */
-
-CREATE TEMPORARY TABLE Standard_price(
-	standard_price_active BOOLEAN,
+CREATE TABLE Standard_price(
+	standard_price_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
     sibling_discount INT,
     student_individual_beginner INT,
     student_individual_intermidiate INT,
@@ -205,20 +38,181 @@ CREATE TEMPORARY TABLE Standard_price(
     instructor_ensemble INT
 );
 
+/* ------------------- People ----------------- */
 
-/* ------------------------ historical database ----------------- */
-
-/* tba:
-
-CREATE TYPE lesson_types AS ENUM ('group', 'individual', 'ensemble');
-CREATE TEMPORARY TABLE Historical_database (
-		lesson_type lesson_types NOT NULL,
-		genre VARCHAR(50),
-		instrument VARCHAR(50),
-		lesson_price INT,
-		student_fname VARCHAR(50),
-		student_lname VARCHAR (50),
-		student_mail_address VARCHAR(50)
+CREATE TABLE Student(
+	student_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+	student_fname VARCHAR(50),
+	student_lname VARCHAR(50),
+	student_address VARCHAR(50),
+	student_mail_address VARCHAR(50),
+	student_phone_number VARCHAR(50),
+	student_sibling BOOLEAN
 );
 
-*/
+CREATE TABLE Instructor(
+	instructor_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+	instructor_fname VARCHAR(50),
+	instructor_lname VARCHAR(50),
+	instructor_address VARCHAR(50),
+	instructor_mail_address VARCHAR(50),
+	instructor_phone_number VARCHAR(50)
+);
+
+/* --- details with people ---*/
+
+CREATE TABLE Guardian_contact_details(
+	student_id_id INT NOT NULL PRIMARY KEY,
+	guardian_fname VARCHAR(50),
+	guardian_lname VARCHAR(50),
+	guardian_mail_address VARCHAR(50),
+	guardian_phone_number VARCHAR(50),
+
+	CONSTRAINT guardian_student FOREIGN KEY(student_id_id)
+        REFERENCES Student(student_id)
+);
+
+/* student_sibling_group identifies groups of siblings */
+
+CREATE TABLE Student_sibling(
+	student_id_id INT NOT NULL PRIMARY KEY,
+	student_sibling_group SERIAL,
+
+	CONSTRAINT student_sibling_grouping FOREIGN KEY(student_id_id)
+        REFERENCES Student(student_id)
+);
+	
+/* --------------------------- Instrument ---------------------------*/
+
+CREATE TABLE Instrument_type(
+	instrument_type_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+	instrument_type_name VARCHAR(50)
+);
+
+/* interactions */
+
+CREATE TABLE Instructor_instrument(
+	instructor_id_id INT,
+	instrument_type_id_id INT,
+	difficulty_level_id_id INT,
+
+	CONSTRAINT instructor_instrument FOREIGN KEY(instructor_id_id)
+        REFERENCES Instructor(instructor_id),
+	CONSTRAINT instructor_instrument_type FOREIGN KEY(instrument_type_id_id)
+        REFERENCES Instrument_type(instrument_type_id),
+	CONSTRAINT instructor_instrument_difficulty FOREIGN KEY(difficulty_level_id_id)
+        REFERENCES Difficulty_level(difficulty_level_id)
+);
+
+CREATE TABLE Rent_instrument_assortment(
+	instrument_type_id_id INT PRIMARY KEY NOT NULL,
+	instrument_type_stored_quantity INT,
+	instrument_type_rent_cost INT
+);
+
+CREATE TABLE Rented_instrument(
+	student_id_id INT NOT NULL,
+	instrument_type_id_id INT NOT NULL,
+	rented_instrument_since TIMESTAMP NOT NULL,
+	rented_instrument_turned_in TIMESTAMP,
+
+	CONSTRAINT student_renting_instrument FOREIGN KEY(student_id_id)
+        REFERENCES Student(student_id),
+	CONSTRAINT instructor_instrument_type FOREIGN KEY(instrument_type_id_id)
+        REFERENCES Instrument_type(instrument_type_id)
+);
+
+/* ------------------------ lesson / ensemble ---------------------- */
+
+/* individual lessons */
+
+CREATE TABLE Individual_lesson(
+	individual_lesson_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+	instructor_id_id INT,
+	instrument_type_id_id INT,
+	difficulty_level_id_id INT,
+	student_id_id INT,
+	standard_price_id_id INT,
+	individual_lesson_start_time TIMESTAMP NOT NULL,
+	individual_lesson_end_time TIMESTAMP,
+
+	CONSTRAINT instructor_id_id FOREIGN KEY(instructor_id_id)
+        REFERENCES Instructor(instructor_id),
+	CONSTRAINT instrument_type_id_id FOREIGN KEY(instrument_type_id_id)
+        REFERENCES Instrument_type(instrument_type_id),
+	CONSTRAINT difficulty_level_id_id FOREIGN KEY(difficulty_level_id_id)
+        REFERENCES Difficulty_level(difficulty_level_id),
+	CONSTRAINT student_id_id FOREIGN KEY(student_id_id)
+        REFERENCES Student(student_id),
+	CONSTRAINT standard_price_id_id FOREIGN KEY(STANDARD_PRICE_ID_ID)
+		references Standard_price(standard_price_id)
+);
+
+/* group lessons */
+
+CREATE TABLE Group_lesson(
+	group_lesson_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+	instructor_id_id INT,
+	instrument_type_id_id INT,
+	difficulty_level_id_id INT,
+	standard_price_id_id INT,
+	group_lesson_start_time TIMESTAMP NOT NULL,
+	group_lesson_end_time TIMESTAMP,
+	group_lesson_student_price INT,
+
+	CONSTRAINT instructor_id_id FOREIGN KEY(instructor_id_id)
+        REFERENCES Instructor(instructor_id),
+	CONSTRAINT instrument_type_id_id FOREIGN KEY(instrument_type_id_id)
+        REFERENCES Instrument_type(instrument_type_id),
+	CONSTRAINT difficulty_level_id_id FOREIGN KEY(difficulty_level_id_id)
+        REFERENCES Difficulty_level(difficulty_level_id),
+	CONSTRAINT standard_price_id_id FOREIGN KEY(STANDARD_PRICE_ID_ID)
+		references Standard_price(standard_price_id)
+);
+
+CREATE TABLE Group_lesson_enrollment(
+	group_lesson_id_id INT NOT NULL,
+	student_id_id INT NOT NULL,
+
+	CONSTRAINT group_lesson_id_id FOREIGN KEY(group_lesson_id_id)
+        REFERENCES Group_lesson(group_lesson_id),
+	CONSTRAINT student_id_id FOREIGN KEY(student_id_id)
+        REFERENCES Student(student_id)
+
+);
+
+/* ensembe */
+
+CREATE TABLE Ensemble_type(
+	ensemble_type_id INT PRIMARY KEY GENERATED ALWAYS AS IDENTITY NOT NULL,
+	ensemble_type_name VARCHAR(50),
+	ensemble_description VARCHAR(1000)
+);
+
+CREATE TABLE Ensemble(
+	ensemble_id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY NOT NULL,
+	instructor_id_id INT NOT NULL,
+	standard_price_id_id INT,
+	ensemble_type_id_id INT NOT NULL,
+	ensemble_start_time TIMESTAMP NOT NULL,
+	ensemble_end_time TIMESTAMP,
+	ensemble_instructor_price INT,
+	ensemble_max_students INT,
+
+	CONSTRAINT instructor_id_id FOREIGN KEY(instructor_id_id)
+        REFERENCES Instructor(instructor_id),
+	CONSTRAINT ensemble_type_id_id FOREIGN KEY(ensemble_type_id_id)
+        REFERENCES Ensemble_type(ensemble_type_id),
+	CONSTRAINT standard_price_id_id FOREIGN KEY(STANDARD_PRICE_ID_ID)
+		references Standard_price(standard_price_id)
+);
+
+CREATE TABLE Ensemble_enrollment(
+	ensemble_lesson_id_id INT NOT NULL,
+	student_id_id INT NOT NULL,
+
+	CONSTRAINT ensemble_lesson_id_id FOREIGN KEY(ensemble_lesson_id_id)
+        REFERENCES Ensemble(ensemble_id),
+	CONSTRAINT student_id_id FOREIGN KEY(student_id_id)
+        REFERENCES Student(student_id)
+);
--- IV1351_queries_list.sql
+++ IV1351_queries_list.sql
@@ -1,33 +1,42 @@
-/* --------------------------------- lesson per month ------------------------------------- */
+/* --------------------------------- query 1: lesson per month ------------------------------------- */
 
 WITH constants AS (
        SELECT 2023 AS desired_year
-)
-SELECT month, SUM(lesson_count) AS total_lessons
-FROM (
-    SELECT EXTRACT(MONTH FROM individual_lesson_start_time) AS month,
-           COUNT(*) AS lesson_count
-    FROM Individual_lesson
+),
+individual_lessons AS (
+    SELECT EXTRACT(MONTH FROM individual_lesson_start_time) AS month, COUNT(*) AS count
+    FROM individual_lesson
     WHERE EXTRACT(YEAR FROM individual_lesson_start_time) = (SELECT desired_year FROM constants)
     GROUP BY EXTRACT(MONTH FROM individual_lesson_start_time)
-    UNION ALL
-    SELECT EXTRACT(MONTH FROM group_lesson_start_time) AS month,
-           COUNT(*) AS lesson_count
-    FROM Group_lesson
+), 
+group_lessons AS (
+    SELECT EXTRACT(MONTH FROM group_lesson_start_time) AS month, COUNT(*) AS count
+    FROM group_lesson
     WHERE EXTRACT(YEAR FROM group_lesson_start_time) = (SELECT desired_year FROM constants)
     GROUP BY EXTRACT(MONTH FROM group_lesson_start_time)
-    UNION ALL
-    SELECT EXTRACT(MONTH FROM ensemble_start_time) AS month,
-           COUNT(*) AS lesson_count
-    FROM Ensemble
+),
+ensembles AS (
+    SELECT EXTRACT(MONTH FROM ensemble_start_time) AS month, COUNT(*) AS count
+    FROM ensemble
     WHERE EXTRACT(YEAR FROM ensemble_start_time) = (SELECT desired_year FROM constants)
     GROUP BY EXTRACT(MONTH FROM ensemble_start_time)
-) AS subquery
-GROUP BY month
-ORDER BY month;
+)
+SELECT 
+    COALESCE(i.month, g.month, e.month) AS month,
+    COALESCE(i.count, 0) + COALESCE(g.count, 0) + COALESCE(e.count, 0) AS total,
+    COALESCE(i.count, 0) AS individual_lessons,
+    COALESCE(g.count, 0) AS group_lessons,
+    COALESCE(e.count, 0) AS ensembles
+FROM 
+    individual_lessons i
+FULL OUTER JOIN 
+    group_lessons g ON i.month = g.month
+FULL OUTER JOIN 
+    ensembles e ON COALESCE(i.month, g.month) = e.month
+ORDER BY 
+    month;
 
-
-/* --------------------------------- sibling count ------------------------------------- */
+/* --------------------------------- query 2: sibling count ------------------------------------- */
 
 SELECT sibling_count, COUNT(*) AS student_count
 FROM (
@@ -38,17 +47,17 @@ FROM (
 GROUP BY sibling_count
 ORDER BY sibling_count;
 
+/* --------------------------------- query 3: instructor lesson per month  ------------------------------------- */
 
-/* --------------------------------- instructor lesson per month  ------------------------------------- */
+-- query:
 
--- query
 WITH constants AS (
 	SELECT 
-              (SELECT EXTRACT(YEAR FROM CURRENT_TIMESTAMP) AS current_year) AS desired_year,
-              (SELECT EXTRACT(MONTH FROM CURRENT_TIMESTAMP) AS current_month) AS desired_month, 
-              1 AS desired_minimum_lesson
+              2023 AS desired_year,
+              7 AS desired_month, 
+              0 AS desired_minimum_lesson
 )
-SELECT instructor_id_id, COUNT(*) AS entry_count
+SELECT instructor_id_id, instructor_fname, instructor_lname, COUNT(*) AS lesson_count
 FROM (
 	SELECT instructor_id_id, individual_lesson_start_time FROM Individual_lesson
 	WHERE EXTRACT(MONTH FROM individual_lesson_start_time) = (SELECT desired_month FROM constants)
@@ -62,39 +71,70 @@ FROM (
 	WHERE EXTRACT(MONTH FROM ensemble_start_time) = (SELECT desired_month FROM constants)
 	AND EXTRACT(YEAR FROM ensemble_start_time) = (SELECT desired_year FROM constants)
 )
-GROUP BY instructor_id_id
+LEFT JOIN Instructor ON instructor_id_id = Instructor.instructor_id
+GROUP BY instructor_id_id, instructor_fname, instructor_lname
 HAVING COUNT(*) > (SELECT desired_minimum_lesson FROM constants)
-ORDER BY entry_count DESC;
+ORDER BY lesson_count DESC;
 
--- verify
-SELECT instructor_id_id, individual_lesson_start_time FROM Individual_lesson
+
+-- verify prior query:
+
+WITH constants AS (
+       SELECT 
+              2023 AS desired_year, 
+              7 AS desired_month, 
+              0 AS desired_minimum_lesson
+)
+       SELECT instructor_id_id, individual_lesson_start_time 
+       FROM Individual_lesson
+       WHERE EXTRACT(MONTH FROM individual_lesson_start_time) = (SELECT desired_month FROM constants)
+       AND EXTRACT(YEAR FROM individual_lesson_start_time) = (SELECT desired_year FROM constants)
 UNION ALL
-SELECT instructor_id_id, group_lesson_start_time FROM Group_lesson
+       SELECT instructor_id_id, group_lesson_start_time 
+       FROM Group_lesson
+       WHERE EXTRACT(MONTH FROM group_lesson_start_time) = (SELECT desired_month FROM constants)
+       AND EXTRACT(YEAR FROM group_lesson_start_time) = (SELECT desired_year FROM constants)
 UNION ALL
-SELECT instructor_id_id, ensemble_start_time FROM Ensemble;
+       SELECT instructor_id_id, ensemble_start_time 
+       FROM Ensemble
+       WHERE EXTRACT(MONTH FROM ensemble_start_time) = (SELECT desired_month FROM constants)
+       AND EXTRACT(YEAR FROM ensemble_start_time) = (SELECT desired_year FROM constants)
+ORDER BY instructor_id_id;
 
 
+/* --------------------------------- query 4: ensembles next week ------------------------------------- */
 
-/* --------------------------------- ensembles next week ------------------------------------- */
+-- query:
 
 WITH constant AS(
 	SELECT 8 AS desired_week
 )
-SELECT -- e.ensemble_id,
-       TO_CHAR(e.ensemble_start_time, 'FMDay') AS day_of_week, -- debugg: e.ensemble_start_time
+SELECT 
+       TO_CHAR(e.ensemble_start_time, 'FMDay') AS day_of_week, 
 	et.ensemble_type_name,
-       (e.ensemble_max_students - COUNT(*)) AS remaining_slots
+       CASE
+              WHEN e.ensemble_max_students - COUNT(ee.ensemble_lesson_id_id) <= 0 THEN 'No seats'
+              WHEN e.ensemble_max_students - COUNT(ee.ensemble_lesson_id_id) BETWEEN 1 AND 2 THEN '1 or 2 seats'
+              ELSE 'Many seats'
+       END AS remaining_slots
 FROM Ensemble e
 LEFT JOIN Ensemble_enrollment ee ON e.ensemble_id = ee.ensemble_lesson_id_id
 LEFT JOIN Ensemble_type et ON e.ensemble_type_id_id = et.ensemble_type_id
 WHERE DATE_PART('week', e.ensemble_start_time) = (SELECT desired_week FROM constant)
 GROUP BY e.ensemble_start_time, e.ensemble_max_students, et.ensemble_type_name;
 
+-- debugg:
 
-/* ------------------------------------ historical database --------------------------------- */
-
-/* tba
-SELECT 'individual', indv.
-FROM Individual_lesson indv
-*/
-
+WITH constant AS(
+	SELECT 8 AS desired_week
+)
+SELECT
+       e.ensemble_id,
+       TO_CHAR(e.ensemble_start_time, 'FMDay') AS day_of_week,
+	et.ensemble_type_name,
+       (e.ensemble_max_students - COUNT(*)) AS remaining_slots
+FROM Ensemble e
+LEFT JOIN Ensemble_enrollment ee ON e.ensemble_id = ee.ensemble_lesson_id_id
+LEFT JOIN Ensemble_type et ON e.ensemble_type_id_id = et.ensemble_type_id
+WHERE DATE_PART('week', e.ensemble_start_time) = (SELECT desired_week FROM constant)
+GROUP BY e.ensemble_start_time, e.ensemble_max_students, et.ensemble_type_name, e.ensemble_id;
